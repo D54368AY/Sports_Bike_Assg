4f85f2213b53bdbb54a55f8cc29c7282b4f27acb
D54368AY <saurabh.dubey@neosoftmail.com>
2021-09-21 20:39:34 +0530

    commited

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+ $(document).ready(function () { 
+    $('.parallax').parallax();
+    $(".sidenav").sidenav();
+    $(".slider").slider();
+    $(".mycars").carousel({    
+      shift : 150 
+    });
+})
